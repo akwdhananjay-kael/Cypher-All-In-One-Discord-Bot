b9fd25c434c6115f75d069f6917af3a322e0fafd
akwdhananjay-kael <akwdhananjay@gmail.com>
2026-07-03 19:35:41 +0500

    Update import statement for config module

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from core.Cypher import Cypher
-from utils.config import whCL, TOKEN
+from utils.config import wCL
 from discord.ext.commands import Context
 from discord.ext import commands
 import discord
